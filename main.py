d776e6a9230e25ca6293a104867dcd3c30b5b316
Luca Cherubin <Kerruba@users.noreply.github.com>
2025-01-17 12:05:19 +0100

    chore(ci): adds ability to remove environment associated with branch deployment

--- main.py
+++ main.py
@@ -6,19 +6,32 @@ from datetime import datetime
 import requests
 
 
-def delete_deployment(id):
+def delete_deployment(repo, id):
     resp = requests.delete(
-        f"https://api.github.com/repos/camunda/operate/deployments/{id}",
+        f"https://api.github.com/repos/{repo}/deployments/{id}",
         headers={"Authorization": f"Bearer {API_KEY}"},
     )
     print(resp.text)
     return resp.status_code
 
 
+def delete_environment(repo, environment_name):
+    resp = requests.delete(
+        f"https://api.github.com/repos/{repo}/environments/{environment_name}",
+        headers={"Authorization": f"Bearer {API_KEY}"},
+    )
+    print(resp.text)
+    return resp.status_code
+
+
+# GH_APP_TOKEN will require repository write permission and admistration permission
+# To being able to delete deployments (and environment)
 API_KEY = os.getenv("GH_APP_TOKEN")
 
 if API_KEY is None:
     raise ValueError("Please define API_KEY as environment var!")
+
+DELETE_ENVIRONMENTS = os.getenv("DELETE_ENVIRONMENTS") or None
 
 repositories = [
     "camunda/operate",  # Operate
@@ -33,12 +46,11 @@ repositories = [
 auth = Auth.Token(API_KEY)
 g = Github(auth=auth)
 
-for r in repositories:
+for repo_name in repositories:
 
-    repo = g.get_repo(r)
+    repo = g.get_repo(repo_name)
 
     deployments = repo.get_deployments()
-
     count = {"active": 0, "deleted": 0, "deactivated": 0}
 
     for depo in deployments:
@@ -59,7 +71,7 @@ for r in repositories:
                 continue
             else:
                 print("Deleting inactive stable / master deployment")
-                delete_deployment(depo.id)
+                delete_deployment(repo_name, depo.id)
                 count["deleted"] += 1
         else:
             print(f"Checking whether branch {branch} still exists")
@@ -72,19 +84,24 @@ for r in repositories:
                 print(f"GitHub branch {branch} doesnt exist anymore")
                 print(f"Deleting GH Deployment - {name}")
 
-                status_code = delete_deployment(depo.id)
+                status_code = delete_deployment(repo_name, depo.id)
 
                 # 422 - We cannot delete an active deployment unless it is the only deployment in a given environment.
                 if status_code == 422:
                     depo.create_status(state="inactive", auto_inactive=True)
-                    delete_deployment(depo.id)
+                    delete_deployment(repo_name, depo.id)
                     count["deactivated"] += 1
+
+                if DELETE_ENVIRONMENTS == "true":
+                    environment_name = depo.environment
+                    print(f"Deleting GH Environment - {environment_name}")
+                    delete_environment(repo_name, environment_name)
 
                 count["deleted"] += 1
 
-    print(f"Finished processing deployments for repository {REPOSITORY}")
-    print("========================================")
-    print("Summary:")
-    print("Active Deployments: ", count["active"])
-    print("Deleted Deployments: ", count["deleted"])
-    print("Deactivated Deployments: ", count["deactivated"])
+        print(f"Finished processing deployments for repository {repo_name}")
+        print("========================================")
+        print("Summary:")
+        print("Active Deployments: ", count["active"])
+        print("Deleted Deployments: ", count["deleted"])
+        print("Deactivated Deployments: ", count["deactivated"])
